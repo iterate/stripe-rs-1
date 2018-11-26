383b375e5a17304b73a09c152ce1292adba44552
Lars Barlindhaug <lars.barlindhaug@iterate.no>
2018-11-26 14:28:19 +0100

    Create fns for creating and reversing transfers

--- src/resources/transfer.rs
+++ src/resources/transfer.rs
@@ -1,5 +1,22 @@
+use client::Client;
+use error::Error;
 use params::{List, Metadata, Timestamp};
 use resources::Currency;
+
+/// The set of parameters that can be used when creating a transfer.
+///
+/// For more details see https://stripe.com/docs/api/transfer_reversals/create
+#[derive(Default, Serialize)]
+pub struct TransferReversalParams {
+    pub id: String,
+    pub amount: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub metadata: Option<Metadata>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub refund_application_fee: Option<bool>,
+}
 
 /// The resource representing a Stripe transfer reversal.
 ///
@@ -9,11 +26,31 @@ pub struct TransferReversal {
     pub id: String,
     pub object: String,
     pub amount: u64,
-    pub balance_transaction: String,
+    pub balance_transaction: Option<String>,
     pub created: Timestamp,
     pub currency: Currency,
     pub metadata: Metadata,
     pub transfer: String,
+}
+
+/// The set of parameters that can be used when creating or updating a transfer.
+///
+/// For more details see https://stripe.com/docs/api/transfer/create and https://stripe.com/docs/api/transfer/update.
+#[derive(Default, Serialize)]
+pub struct TransferParams {
+    pub amount: u64,
+    pub currency: String,
+    pub destination: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub description: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub metadata: Option<Metadata>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub source_transaction: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub source_type: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transfer_group: Option<String>,
 }
 
 /// The resource representing a Stripe transfer.
@@ -35,7 +72,27 @@ pub struct Transfer {
     pub metadata: Metadata,
     pub reversals: List<TransferReversal>,
     pub reversed: bool,
-    pub source_transaction: String,
+    pub source_transaction: Option<String>,
     pub source_type: String,
-    pub transfer_group: String,
+    pub transfer_group: Option<String>,
+}
+
+impl Transfer {
+    /// Creates a new transfer.
+    ///
+    /// For more details see https://stripe.com/docs/api/transfer/create.
+    pub fn create(client: &Client, params: TransferParams) -> Result<Transfer, Error> {
+        client.post("/transfers", params)
+    }
+
+    /// Reverses a transfer.
+    ///
+    /// For more details see https://stripe.com/docs/api/transfer_reversals/create.
+    pub fn reverse(
+        client: &Client,
+        transfer_id: &str,
+        params: TransferReversalParams,
+    ) -> Result<TransferReversal, Error> {
+        client.post(&format!("/transfers/{}/reversals", transfer_id), params)
+    }
 }
